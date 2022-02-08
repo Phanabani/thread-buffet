74b91bd60e60b279b400ba15e1f068ef43fb728b
Phanabani <phanabani@gmail.com>
2022-02-08 00:34:12 -0500

    Add type annotations.

--- src/common/file.ts
+++ src/common/file.ts
@@ -1,6 +1,6 @@
 import { dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
-export function getDirnameFromURL(url) {
+export function getDirnameFromURL(url: string): string {
     return dirname(fileURLToPath(url));
 }
