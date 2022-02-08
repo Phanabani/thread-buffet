11160fa59e7f580c23f3e151122a1999b475885a
Phanabani <phanabani@gmail.com>
2022-02-08 00:54:07 -0500

    Annotate interaction type.

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,3 +1,4 @@
+import { BaseCommandInteraction } from 'discord.js';
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { DiscordCommandHandler } from '../types/discordCommandHandler';
 
@@ -5,7 +6,7 @@ export default <DiscordCommandHandler>{
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
-	async execute(interaction) {
+	async execute(interaction: BaseCommandInteraction) {
 		await interaction.reply('Pong!');
 	},
 };
