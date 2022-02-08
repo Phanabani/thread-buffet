8d54adeed67dcebaae1d75be9794dd1988139ced
Phanabani <phanabani@gmail.com>
2022-02-08 00:45:17 -0500

    Minor typing fix.

--- src/common/dynamicImport.ts
+++ src/common/dynamicImport.ts
@@ -2,13 +2,13 @@ import * as fs from 'node:fs';
 import * as path from 'node:path';
 
 export async function importAllDefault<ImportType>(directory): Promise<ImportType[]> {
-	const commands = [];
+	const commands: ImportType[] = [];
 	const commandFiles = fs.readdirSync(directory).filter(file => file.endsWith('.js')
 	);
 	for (const file of commandFiles) {
 		const importPath = path.join(directory, file);
 		const command = (await import(`file://${importPath}`));
-		commands.push(command.default);
+		commands.push((command.default as ImportType));
 	}
 	return commands;
 }
