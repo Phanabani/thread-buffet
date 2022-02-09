d07136814ca9adf235251301ffe326e242ce9390
Phanabani <phanabani@gmail.com>
2022-02-09 01:26:45 -0500

    Add setThreadChannel command.

---
+++ src/commands/setThreadChannel.ts
@@ -0,0 +1,35 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { ChannelType } from 'discord-api-types';
+import { BaseCommandInteraction, CommandInteractionOptionResolver, TextBasedChannel, TextChannel } from 'discord.js';
+import { PERMISSIONS_THREAD_COMMANDS } from '../common/constants.js';
+import { checkPerms } from '../common/discord.js';
+import { setThreadChannel } from '../database/database.js';
+import { DiscordCommandHandler } from '../types/discordCommandHandler';
+
+async function execute(interaction: BaseCommandInteraction) {
+    const i = interaction;
+    const options = i.options as CommandInteractionOptionResolver;
+
+    if (!await checkPerms(i, PERMISSIONS_THREAD_COMMANDS)) return;
+
+    const channel = (options.getChannel('channel') || i.channel) as TextChannel;
+    await setThreadChannel(i.guildId!, channel.id);
+
+    await i.reply({
+        content: `Set thread channel to <#${channel.id}>`,
+        ephemeral: true
+    });
+}
+
+export default <DiscordCommandHandler>{
+    data: new SlashCommandBuilder()
+        .setName('set-thread-channel')
+        .setDescription('Set the channel where active threads will be shared.')
+        .addChannelOption(option =>
+            option.setName('channel')
+                .setDescription('The channel to write active threads in.')
+                .setRequired(false)
+                .addChannelType(ChannelType.GuildText)
+        ),
+    execute
+};
