32c0e52b7c49cb8dc8ef7c10b9c8c8cba2deb1cc
Phanabani <phanabani@gmail.com>
2022-02-10 18:48:13 -0500

    Add thread filtering to ignore archived threads and those not visible to viewAsRole.

--- src/messageWriter.ts
+++ src/messageWriter.ts
@@ -1,10 +1,31 @@
 import { Embed } from '@discordjs/builders';
-import { Client, Collection, Guild, ThreadChannel } from 'discord.js';
+import { Client, Collection, Guild, ThreadChannel, Permissions, RoleResolvable } from 'discord.js';
 import { COLOR_THREADS_INFO } from './common/constants.js';
 import { fetchChannel, fetchMessage } from './common/discord.js';
-import { getThreadChannel, getThreadMessage, setThreadMessage } from './database/database.js';
+import { getThreadChannel, getThreadMessage, getViewAsRole, setThreadMessage } from './database/database.js';
 
-function createThreadsEmbed(threads: Collection<string, ThreadChannel> | null): Embed {
+function prepareThreads(
+    threads: Collection<string, ThreadChannel>,
+    viewAsRole: RoleResolvable | null | undefined = undefined
+): Collection<string, ThreadChannel> {
+
+    function filterVisibility(t: ThreadChannel): boolean {
+        let shouldPass = true;
+
+        // Remove if archived or archival status is null
+        shouldPass &&= !(t.archived ?? true);
+
+        // Remove if the specified role (or @everyone)
+        let rolePerms: Readonly<Permissions> | null = null;
+        if (viewAsRole) {
+            rolePerms = t.permissionsFor(viewAsRole);
+        }
+        const everyonePerms = t.permissionsFor(t.guild.roles.everyone);
+        shouldPass &&= (rolePerms || everyonePerms).has(Permissions.FLAGS.VIEW_CHANNEL);
+
+        return shouldPass;
+    }
+
     function compareThreadNames(t1: ThreadChannel, t2: ThreadChannel): number {
         return t1.name.localeCompare(t2.name);
     }
@@ -15,15 +36,28 @@ function createThreadsEmbed(threads: Collection<string, ThreadChannel> | null): 
         return parentName1.localeCompare(parentName2);
     }
 
+    let newThreads = threads.clone();
+
+    // Remove archived or non-visible threads
+    newThreads = newThreads.filter(filterVisibility);
+
+    // Sort by thread name, then by their parent threads
+    newThreads.sort(compareThreadNames);
+    newThreads.sort(compareThreadParentNames);
+    return newThreads;
+}
+
+function createThreadsEmbed(
+    threads: Collection<string, ThreadChannel> | null,
+    viewAsRole: RoleResolvable | null | undefined = undefined
+): Embed {
     let content = '';
     if (threads) {
         const contentParts: string[] = [];
         let lastParentName: string | null = null;
-        // Sort by thread name, then by their parent threads
-        const sortedThreads = threads.sorted(compareThreadNames);
-        sortedThreads.sort(compareThreadParentNames);
+        const preparedThreads = prepareThreads(threads, viewAsRole);
 
-        for (const [id, thread] of sortedThreads) {
+        for (const [id, thread] of preparedThreads) {
             let parentName = thread.parent?.name;
             if (parentName && parentName !== lastParentName) {
                 if (lastParentName !== null) contentParts.push('');
@@ -79,6 +113,7 @@ export async function onThreadChannelDidChange(client: Client, guild: Guild) {
 export async function onThreadsUpdated(client: Client, guild: Guild) {
     const channelId = await getThreadChannel(guild.id);
     const msgId = await getThreadMessage(guild.id);
+    const viewAsRole = await getViewAsRole(guild.id);
     if (!(channelId && msgId)) return;
     const msg = await fetchMessage(client, channelId, msgId);
     if (!msg) return;
@@ -88,6 +123,6 @@ export async function onThreadsUpdated(client: Client, guild: Guild) {
     if (threads.hasMore) {
         console.warn('fetchActiveThreads.hasMore is true. idk what this means.');
     }
-    const embed = createThreadsEmbed(threads.threads);
+    const embed = createThreadsEmbed(threads.threads, viewAsRole);
     await msg.edit({ embeds: [embed] })
 }
